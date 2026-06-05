5b2bfc72a0adf0e15e5277737e271379d0e6ce96
reaperXKSGIV <ollie2024frearson@hotmail.com>
2026-06-05 16:15:19 +0100

    Update gamelist.lua

--- Moon Hub/gamelist.lua
+++ Moon Hub/gamelist.lua
@@ -1,16 +1,5 @@
 local Games = {
-  [994732206] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/Blox%20Fruit.lua", -- Blox Fruit
-  [1268927906] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/Muscle%20Legends.lua", -- Muscle Legends
-  [3808081382] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/The%20Strongest%20Battleground.lua",  -- The Strongest Battlegrounds
-  [6401952734] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/Pet%20GO.lua", -- Pet GO
-  [5750914919] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/Fisch.lua", -- Fisch
-  [7436755782] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/refs/heads/main/Grow%20a%20Garden.lua", -- Grow a Garden
-  [7671049560] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/refs/heads/main/The%20Forge.lua", -- The Forge
-  [9186719164] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/refs/heads/main/Sailor%20Piece.lua", -- Sailor Piece
-  [9792947201] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/refs/heads/main/Slime%20RNG.lua", -- Slime RNG
-  [9098570654] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/refs/heads/main/Survive%20the%20Apocalypse.lua", -- Survive the Apocalypse
-  [10004244222] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/refs/heads/main/Kick%20a%20Lucky%20Block.lua", -- Kick a Lucky Block
-	[10039338037] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/refs/heads/main/Build%20A%20Ring%20Farm.lua", -- Build A Ring Farm
+  [1268927906] = "", -- Muscle Legends
 }
 
 return Games
